d3b8bd5b4b1d1ae093c5347f08d11ed467ced7e7
Conrad <cnrdcasper@gmail.com>
2020-04-29 02:15:52 -0400

    using graphql with gatsby to query word press api

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,17 +4,33 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import {graphql, StaticQuery} from 'gatsby'
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <StaticQuery query={graphql`
+    {
+      allWordpressPage{
+         edges{
+          node{
+            title
+            content
+          }
+        }
+      }
+    }
+    `} render={props => (
+      <div>
+        {props.allWordpressPage.edges.map(page => (
+          <div key={page.node.id}>
+            <h1>
+              {page.node.title}
+            </h1>
+            <div dangerouslySetInnerHTML={{__html: page.node.content}} />
+          </div>
+        ))}
+      </div>
+    )}/> 
   </Layout>
 )
 
