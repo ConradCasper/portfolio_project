e240104a113c1c3d41b474096132ea386f4a7785
Conrad <cnrdcasper@gmail.com>
2020-05-11 12:22:12 -0400

    rendering site metadata in the header

--- src/components/MainMenu.js
+++ src/components/MainMenu.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import { graphql, StaticQuery, Link } from 'gatsby';
 import styled from 'styled-components';
+import SiteInfo from './Siteinfo'
 
 const MainMenuWrapper = styled.div`
     display: flex;
     background-color: rgb(3, 27, 77);
+`
+
+const MenuItem = styled(Link)`
+    color: white;
+    display: block;
+    padding: 8px 16px;
 `
 
 const MainMenu = () => (
@@ -28,10 +35,11 @@ const MainMenu = () => (
       }
     `} render={props => (
         <MainMenuWrapper>
+            <SiteInfo/>
             {props.allWordpressWpApiMenusMenusItems.edges[0].node.items.map(item => (
-                <Link to={item.object_slug} key={item.title}>
+                <MenuItem to={item.object_slug} key={item.title}>
                     {item.title}
-                </Link>
+                </MenuItem>
             ))}
         </MainMenuWrapper>
     )}/>
