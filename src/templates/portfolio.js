5a95e4c8c858bcd545506089556d1d0fbdc07d40
Conrad <cnrdcasper@gmail.com>
2020-06-30 12:17:43 -0400

    implemented responsive styling for embedded youtube videos with styled-components

--- src/templates/portfolio.js
+++ src/templates/portfolio.js
@@ -1,30 +1,28 @@
 import React from 'react'
 import Layout from '../components/layout'
-// import styled from 'styled-components'
+import styled from 'styled-components'
 
 // const FeaturedImage = styled.img`
 //     max-width: 300px;
 //     margin: 16px 0;
 // `
 
-// const YouTubeVideoWrapper = styled.div`
-//     position: relative;
-//     padding-bottom:56.25%;
-//     padding-top:30px;
-//     height:0;
-//     overflow:hidden;
-// `
 
-// const YouTubeVideo = styled.div`
-//     position:absolute;
-//     top:0;
-//     left:0;
-//     right: 0;
-//     bottom: 0;
-//     max-width:600px;
-//     height:100%;
-//     margin: auto;
-// `
+
+const YouTubeEmbedResponsive = styled.div`
+  position:relative;
+  padding-bottom:56.25%;
+  padding-top:30px;
+  height:0;
+  overflow:hidden;
+  p, iframe, object, embed{
+    position:absolute;
+    top:0;
+    left:0;
+    width:100%;
+    height:100%;
+  }
+`;
 
 
 
@@ -57,8 +55,8 @@ export default ({ pageContext }) => (
             {/* <FeaturedImage src={pageContext.featured_media.source_url} />  */}
         </div>
         <div dangerouslySetInnerHTML={{__html: pageContext.content}}/>
-        <div className="youtube-responsive-container" dangerouslySetInnerHTML={{__html: pageContext.excerpt}} />
-            {/* <YouTubeVideo dangerouslySetInnerHTML={{__html: pageContext.excerpt}} />  */}
-        
+        <YouTubeEmbedResponsive>
+            {pageContext.excerpt}
+        </YouTubeEmbedResponsive>
     </Layout>
 )
