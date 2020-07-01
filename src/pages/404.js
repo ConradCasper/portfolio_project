ba2c0e711b281dbd761fa0af090f14c5b1a484bd
Conrad <cnrdcasper@gmail.com>
2020-06-30 20:05:22 -0400

    trying to solve flashing 404 before redirect

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,17 @@
 import React from "react"
-
-import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
-)
+const browser = typeof window !== "undefined" && window;
 
-export default NotFoundPage
+const NotFoundPage = () => {
+  return (
+    browser && (
+      <div>
+        <SEO title="404 Page not found" />
+        <h1>404 Error content...</h1>
+      </div>
+    )
+  );
+};
+
+export default NotFoundPage;
