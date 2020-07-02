4c793ebb178bed6325d4e84197e7137861385f6c
Conrad <cnrdcasper@gmail.com>
2020-07-02 16:48:37 -0400

    i forgot what changes i made lol

--- src/templates/page.js
+++ src/templates/page.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Layout from '../components/layout';
 
 
+
+
 export default ({ pageContext }) => (
     <Layout>
         <h1 dangerouslySetInnerHTML={{__html: pageContext.title}} />
