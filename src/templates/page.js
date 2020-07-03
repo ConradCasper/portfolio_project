3c61729892818e559b14ff4d41b3cf7ad708cd12
Conrad <cnrdcasper@gmail.com>
2020-07-02 21:37:53 -0400

    have the fade-in on the home page somewhat how I want it.

--- src/templates/page.js
+++ src/templates/page.js
@@ -1,12 +1,47 @@
 import React from 'react';
 import Layout from '../components/layout';
+import styled, { keyframes } from 'styled-components';
 
+const fadeIn = keyframes`
+    0% {
+        opacity: 0;
+        transform: translateX(100px);
+    }
+    100% {
+        opacity: 1;
+        transform: translateX(0);
+    }
+`
 
+const FadeIn = styled.div`
+animation: 5s ${fadeIn} ease-out;
+`
+
+// const ProfileImage = styled.img`
+//     max-width: 235px;
+//     margin: 0;
+//     padding: 0px 16px 16px 0px;
+//     float: left;
+// `
+
+const ContentContainer = styled.div`
+    max-width: 650px;
+   
+    margin: auto;
+    text-align: center;
+`
+
+const Content = styled.h1`
+    display: inline-block;
+`
 
 
 export default ({ pageContext }) => (
     <Layout>
-        <h1 dangerouslySetInnerHTML={{__html: pageContext.title}} />
-        <div dangerouslySetInnerHTML={{__html: pageContext.content}}/>
+        <FadeIn>
+            <ContentContainer>
+                <Content dangerouslySetInnerHTML={{__html: pageContext.content}}/>
+            </ContentContainer>
+        </FadeIn>
     </Layout>
 )
