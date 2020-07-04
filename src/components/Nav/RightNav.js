7fb8f880b6cf7beefce4b9ab54f3b2f39768f155
Conrad <cnrdcasper@gmail.com>
2020-07-04 02:49:00 -0400

    working on the appearance of the burger menu

--- src/components/Nav/RightNav.js
+++ src/components/Nav/RightNav.js
@@ -6,21 +6,29 @@ import styled from 'styled-components';
 const NavItemWrapper = styled.div`
     display: flex;
     flex-flow: row nowrap;
-    // width: 100%
-    justify-content: space-between;
+    
 
     @media (max-width: 768px) {
         flex-flow: column nowrap;
-        background: #0D2538;
+        background: white;
         position: fixed;
         transform: ${({ open }) => open ? 'translateX(0)' : 'translateX(100%)'};
         top: 0;
         right: 0;
         height: 100vh;
-        width: 300px;
+        width: 100%;
         padding-top: 3.5rem;
         transition: transform 0.3s ease-in-out;
         z-index: 10;
+
+        a{
+            color: black;
+            display: block;
+            padding: 16px 32px;
+            text-decoration: bold;
+            font-family: Arial;
+            font-size: 35px;
+        }
     }
 `;
 
