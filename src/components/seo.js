df55915d49466ae1e3cf12b6cf466f7bf7534da9
Conrad <cnrdcasper@gmail.com>
2020-07-14 15:17:08 -0400

    changed attribute "property" to "name" on twitter meta tags

--- src/components/seo.js
+++ src/components/seo.js
@@ -33,12 +33,12 @@ function SEO({ title }) {
       <meta property="og:image" content="http://wp.conradcasper.io/wp-content/uploads/2020/07/Screen-Shot-2020-07-14-at-12.55.03-PM.png"/>
 
       {/* <!-- Twitter --> */}
-      <meta property="twitter:card" content="summary_large_image"/>
-      <meta property="twitter:url" content="https://www.conradcasper.io/"/>
+      <meta name="twitter:card" content="summary_large_image"/>
+      <meta name="twitter:url" content="https://www.conradcasper.io/"/>
       <meta name="twitter:creator" content="@Conrad_Casper"></meta>
-      <meta property="twitter:title" content="Conrad Casper | Full Stack Web Developer"/>
-      <meta property="twitter:description" content="I am a web developer based in beautiful Atlanta, Georgia. I love creating slick frontend designs for businesses and clients looking to attract more customers to their website."/>
-      <meta property="twitter:image" content="http://wp.conradcasper.io/wp-content/uploads/2020/07/Screen-Shot-2020-07-14-at-12.55.03-PM.png"></meta>
+      <meta name="twitter:title" content="Conrad Casper | Full Stack Web Developer"/>
+      <meta name="twitter:description" content="I am a web developer based in beautiful Atlanta, Georgia. I love creating slick frontend designs for businesses and clients looking to attract more customers to their website."/>
+      <meta name="twitter:image" content="http://wp.conradcasper.io/wp-content/uploads/2020/07/Screen-Shot-2020-07-14-at-12.55.03-PM.png"></meta>
     </Helmet>
   )
 }
