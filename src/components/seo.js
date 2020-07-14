67f0a0537382c36aff78217ccbc5b245c84a48da
Conrad <cnrdcasper@gmail.com>
2020-07-14 13:51:58 -0400

    adding site info to react helmet so image shows when i share link

--- src/components/seo.js
+++ src/components/seo.js
@@ -20,7 +20,25 @@ function SEO({ title }) {
 
   return (
     <Helmet 
-    title={ title ? `${title} | ${site.allWordpressSiteMetadata.edges[0].node.name} | ${site.allWordpressSiteMetadata.edges[0].node.description}` : `${site.allWordpressSiteMetadata.edges[0].node.name} | ${site.allWordpressSiteMetadata.edges[0].node.description}`} />
+    title={ title ? `${title} | ${site.allWordpressSiteMetadata.edges[0].node.name} | ${site.allWordpressSiteMetadata.edges[0].node.description}` : `${site.allWordpressSiteMetadata.edges[0].node.name} | ${site.allWordpressSiteMetadata.edges[0].node.description}`} >
+      {/* <!-- Primary Meta Tags --> */}
+      
+      <meta name="description" content="I am a web developer based in beautiful Atlanta, Georgia. I love creating slick frontend designs for businesses and clients looking to attract more customers to their website."/>
+
+      {/* <!-- Open Graph / Facebook --> */}
+      <meta property="og:type" content="website"/>
+      <meta property="og:url" content="https://www.conradcasper.io/"/>
+      <meta property="og:title" content="Conrad Casper | Full Stack Web Developer"/>
+      <meta property="og:description" content="I am a web developer based in beautiful Atlanta, Georgia. I love creating slick frontend designs for businesses and clients looking to attract more customers to their website."/>
+      <meta property="og:image" content="http://wp.conradcasper.io/wp-content/uploads/2020/07/Screen-Shot-2020-07-14-at-12.55.03-PM.png"/>
+
+      {/* <!-- Twitter --> */}
+      <meta property="twitter:card" content="summary_large_image"/>
+      <meta property="twitter:url" content="https://www.conradcasper.io/"/>
+      <meta property="twitter:title" content="Conrad Casper | Full Stack Web Developer"/>
+      <meta property="twitter:description" content="I am a web developer based in beautiful Atlanta, Georgia. I love creating slick frontend designs for businesses and clients looking to attract more customers to their website."/>
+      <meta property="twitter:image" content="http://wp.conradcasper.io/wp-content/uploads/2020/07/Screen-Shot-2020-07-14-at-12.55.03-PM.png"></meta>
+    </Helmet>
   )
 }
 
