41600c0d20936555b5824a875d7e89fd7045b5e6
Conrad <cnrdcasper@gmail.com>
2020-07-13 23:42:49 -0400

    contact page is done! just need to add more descriptions for the portfolio items now!!

--- src/templates/about.js
+++ src/templates/about.js
@@ -3,16 +3,16 @@ import Layout from '../components/layout';
 import SEO from '../components/seo';
 import styled, { keyframes } from 'styled-components';
 
-// const headerFadeIn = keyframes`
-//     0% {
-//         opacity: 0;
-//         transform: translateX(100px);
-//     }
-//     100% {
-//         opacity: 1;
-//         transform: translateX(0);
-//     }
-// `
+const headerFadeIn = keyframes`
+    0% {
+        opacity: 0;
+        transform: translateX(100px);
+    }
+    100% {
+        opacity: 1;
+        transform: translateX(0);
+    }
+`
 
 const contentFadeIn = keyframes`
     0% {
@@ -28,35 +28,37 @@ const contentFadeIn = keyframes`
 
 
 
-// const ProfileImage = styled.img`
-//     max-width: 235px;
-//     margin: 0;
-//     padding: 0px 16px 16px 0px;
-//     float: left;
-// `
+const ProfileImage = styled.img`
+    max-width: 235px;
+    padding: 0px 16px 16px 0px;
+    align-self: center;
+`
 
 const ContentContainer = styled.div`
     max-width: 650px;
-    backspace-visibility: hidden;
+    display: flex;
+    flex-direction: column;
     margin: 0 auto;
-    text-align: center;
+    text-align: auto;
+    align-content: center;
+    justify-content: center;
+    align-self: center;
 `
 
-// const Header = styled.h1`
-//     margin-bottom: 0;
-//     font-size: 50px;
-//     animation: 2s ${headerFadeIn} ease-out;
-    
-// `
+const Header = styled.h1`
+    font-size: 40px;
+    animation: 2s ${headerFadeIn} ease-out;
+    align-self: center
+`
 
 const Content = styled.div`
-    font-size: 25px;
+    font-size: 20px;
     margin: 0;
     animation: 2s ${contentFadeIn} ease-out;
     
-    p {
-        margin-top: 0;
-    }
+    // p {
+    //     margin-top: 0;
+    // }
 `
 
 
@@ -67,7 +69,8 @@ export default ({ pageContext }) => {
     <Layout>
         <SEO title="About" />
             <ContentContainer>
-                {/* <Header dangerouslySetInnerHTML={{__html: pageContext.acf.header}}/> */}
+                <Header dangerouslySetInnerHTML={{__html: pageContext.title}}/>
+                <ProfileImage src={pageContext.featured_media.source_url} />
                 <Content dangerouslySetInnerHTML={{__html: pageContext.content}} />
             </ContentContainer>
     
